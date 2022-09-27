aac871bf2d8f29c4066404b3d65259abb08206bc
José Ortega Márquez <40195860+joseeortega@users.noreply.github.com>
2022-09-27 19:16:00 +0200

    install axios and list pokemons names

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,41 @@
-import { useEffect, useState } from 'react'
-import { getPokemons } from '../services/PokeApi'
+import { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
+import { getPokemons } from "../services/PokeApi";
 
 function Home() {
-  const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState([]);
 
-  const entryStyle = {
-  }
+  const entryStyle = {};
 
   useEffect(() => {
-    getPokemons({limit: 10, offset: 0}).then((pokemons) => {
-      debugger;
-      setPokemons(pokemons)
-    })
-  }, [])
+    getPokemons({ limit: 10, offset: 0 }).then(
+      (
+        response: AxiosResponse<{
+          data: {
+            results: any[];
+          };
+        }>
+      ) => {
+        setPokemons(response.data.results);
+      }
+    );
+  }, []);
 
   return (
     <div id="Home" style={entryStyle}>
       <div>
-      <h1>Pokemon Team Builder</h1>
-      <p>Create your own Pokemon Team easily selecting your favourite competitive Pokemon!</p>
+        <h1>Pokemon Team Builder</h1>
+        <p>
+          Create your own Pokemon Team easily selecting your favourite
+          competitive Pokemon!
+        </p>
       </div>
-      <div>
-        Pokemon total length: {pokemons.length}
-      </div>
-      
+      <div>Pokemon total length: {pokemons.length}</div>
+      {pokemons.map((pokemon, index) => (
+        <p key={index}>{pokemon.name}</p>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
