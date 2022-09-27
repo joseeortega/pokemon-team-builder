266b4a9b86854632f47b660b5f0f99340296e681
José Ortega Márquez <40195860+joseeortega@users.noreply.github.com>
2022-09-27 20:08:08 +0200

    implement disable load screen button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Home from './pages/Home'
-import LoadApp from './pages/LoadApp'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import LoadApp from "./pages/LoadApp";
 
 function App() {
-  const [loading, setLoading] = useState(100)
+  const [loading, setLoading] = useState(0);
 
   useEffect(() => {
     setTimeout(() => {
-      if(loading !== 100){
-        setLoading(loading + 20); 
+      if (loading !== 100) {
+        setLoading(loading + 20);
       }
     }, 500);
   }, [loading]);
 
   return (
-    <main>
-    {
-      loading < 100 ? (<LoadApp loading={loading}/>): (<Home/>)
-    }
-    </main>
-  )
+    <main>{loading < 100 ? <LoadApp loading={loading} /> : <Home />}</main>
+  );
 }
 
-export default App
+export default App;
--- src/pages/LoadApp.tsx
+++ src/pages/LoadApp.tsx
@@ -1,46 +1,46 @@
-import { CSSProperties, useState } from 'react';
+import { CSSProperties, useState } from "react";
 
 const getRandomImage = () => {
-  return RANDOM_IMAGES[getRandomNumber(0, RANDOM_IMAGES.length)]
-}
+  return RANDOM_IMAGES[getRandomNumber(0, RANDOM_IMAGES.length)];
+};
 
 const getRandomNumber = (min: number, max: number) => {
   return Math.floor(Math.random() * max) + min;
-}
+};
 
 const RANDOM_IMAGES = [
-'https://images3.alphacoders.com/127/thumb-1920-127579.jpg',
-'https://wallpapercave.com/wp/wp2763459.jpg',
-'https://i.pinimg.com/originals/7b/79/65/7b79657c3574b2d64a5f0a4d9402b41d.jpg',
-]
+  "https://images3.alphacoders.com/127/thumb-1920-127579.jpg",
+  "https://wallpapercave.com/wp/wp2763459.jpg",
+  "https://i.pinimg.com/originals/7b/79/65/7b79657c3574b2d64a5f0a4d9402b41d.jpg",
+];
 
-function LoadApp(props : {loading: number}) {
-
-  const [randomImage] = useState(getRandomImage())
+function LoadApp(props: { loading: number }) {
+  const [randomImage] = useState(getRandomImage());
 
   const getRandomStyle = () => {
     return {
       ...entryStyle,
       backgroundImage: `url(${randomImage})`,
-    }
-  }
+    };
+  };
 
   const entryStyle: CSSProperties = {
     height: "100vh",
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center"
-  } 
+    justifyContent: "center",
+  };
 
   return (
     <div id="LoadApp" style={getRandomStyle()}>
       <div>
-      <h1>Pokemon Team Builder</h1>
-      <h3>Developed by Jose Ortega Marquez</h3>
-      <p>Loading.. {props.loading}%</p>
+        <h1>Pokemon Team Builder</h1>
+        <h3>Developed by Jose Ortega Marquez</h3>
+        <p>Loading.. {props.loading}%</p>
+        <button>Disable loading screen</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default LoadApp
+export default LoadApp;
